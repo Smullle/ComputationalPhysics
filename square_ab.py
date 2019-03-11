0767aebcaefb21145d7f912342269fe2c424e91f
Shane Smullen <shanesmullen@gmail.com>
2019-03-11 17:12:58 +0000

    Delete square_ab.py

--- square_ab.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-
-#Take input from user and store it as x
-x = int(input('Input a positive number to be squared: '))
-#Take input from user and store it as y
-y = int(input('Input another positive number to be squared: '))
-#Compute X^2 and y^2 and print the result
-print(x, ' squared is ' , x*x ,' and ', y,' squared is ' , y*y)
